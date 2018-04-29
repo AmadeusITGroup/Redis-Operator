7beecc50f5192784d872e0e155e18e7df452c297
cedric lamoriniere <cedric.lamoriniere@amadeus.com>
2018-04-29 22:28:10 +0200

    improves travisci config

--- go.test.sh
+++ go.test.sh
@@ -6,7 +6,7 @@ echo "" > coverage.txt
 for d in $(go list -f '{{if .TestGoFiles}}{{ .ImportPath }}{{end}}' ./... | grep -v vendor | grep -v test/e2e); do
     go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
+        cat profile.out | grep -v zz_generated >> coverage.txt
         rm profile.out
     fi
 done
