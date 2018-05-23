470fd66ecc3a3df157f97c59b09dc38729bc75fe
Cedric Lamoriniere <cedric.lamoriniere@amadeus.com>
2018-05-23 14:55:19 +0200

    add release process and tooling (#10)
    Fixes: #8
    
    Add scripts for releasing the docker images and the helm charts.

--- hack/helm-release.sh
+++ hack/helm-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "please provide the version as parameter: ./helm-release.sh <version>  <chart-name>\n"
+    echo "please provide the version as parameter: ./helm-release.sh <version> <chart-name>\n"
     exit 1
 fi
 
@@ -12,6 +12,6 @@ fi
 
 cd $(git rev-parse --show-toplevel)
 helm package --version "$1" chart/$2
-mv "helm-$2-$1.tgz" docs/
+mv "$2-$1.tgz" "docs/helm-$2-$1.tgz"
 helm repo index docs --url https://amadeusitgroup.github.io/redis-operator/ --merge docs/index.yaml
 git add --all docs/
---
+++ hack/release.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+if [ -z "$1" ]; then
+    echo "please provide the version as parameter: ./release.sh <version> [git_remote]\n"
+    exit 1
+fi
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+GIT_REMOTE="origin"
+if [ -n "$2" ]; then
+    GIT_REMOTE=$2
+fi
+
+zsh $GIT_ROOT/hack/helm-release.sh $1 redis-operator
+zsh $GIT_ROOT/hack/helm-release.sh $1 redis-cluster
+
+# Update CHANGELOG.md file
+ssed -i.bak "5i## Release $1\n" CHANGELOG.md
+
+git commit -am "release $1"
+git tag -f $1
+#git push -f --tags $GIT_REMOTE master
