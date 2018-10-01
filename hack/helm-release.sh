e93715daaa71c54552c02033f4bbced5eb554cba
cedric lamoriniere <cedric.lamoriniere@amadeus.com>
2018-10-01 20:25:30 +0200

    fix and improve release process
    Fixes: #30
    
    - Use same go build parameters for docker release images
    - Update properly the chart before delivering it

--- hack/helm-release.sh
+++ hack/helm-release.sh
@@ -11,7 +11,9 @@ if [ -z "$2" ]; then
 fi
 
 cd $(git rev-parse --show-toplevel)
+sed "s/tag: master/tag: $1/" chart/$2/values.yaml > chart/$2/values.tmp.yaml; mv chart/$2/values.tmp.yaml chart/$2/values.yaml
 helm package --version "$1" chart/$2
 mv "$2-$1.tgz" "docs/helm-$2-$1.tgz"
+git checkout -- chart/$2/values.yaml
 helm repo index docs --url https://amadeusitgroup.github.io/redis-operator/ --merge docs/index.yaml
 git add --all docs/
